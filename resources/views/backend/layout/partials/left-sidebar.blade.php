652e9b1357aaa873f1383ce6047144000d73817e
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2023-09-26 16:37:15 +0600

    book store project mastering for backend and frontend

---
+++ resources/views/backend/layout/partials/left-sidebar.blade.php
@@ -0,0 +1,29 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+      <a class="sidebar-brand" href="index.html">
+        <span class="align-middle">Online Bookstore</span>
+      </a>
+  
+      <ul class="sidebar-nav">
+        <li class="sidebar-header">
+          Pages
+        </li>
+  
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="index.html">
+            <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+          </a>
+        </li>
+  
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="pages-profile.html">
+            <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+          </a>
+        </li>
+  
+      </ul>
+  
+      <div class="sidebar-cta">
+      </div>
+    </div>
+  </nav>
